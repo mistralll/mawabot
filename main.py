5d903923bfc0074169788833daaa46397d45f372
mistralll <me.pooh441@gmail.com>
2024-03-18 12:49:23 +0900

    clean

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ tree = discord.app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    print("起動完了")
+    print("READY")
     await tree.sync()
 
-@client.event # 通話の開始を通知
+# 通話が開始されるとメッセージを送信する
+@client.event
 async def on_voice_state_update(mem:discord.Member, bf:discord.VoiceState, af:discord.VoiceState):
     if bf.channel == None and len(af.channel.members) == 1:
-        print(f'[LOG] {mem.display_name} が {af.channel.name} に参加しました。')
+        print(f'VC_NOTICE: [{mem.display_name}] join in [{af.channel.name}].')
         await client.get_channel(int(os.environ['NOTICE_CHANNEL_ID'])).send(f'{af.channel.name}に誰か来たようです！')
 
+# YouTube ダウンロードコマンド
 from yt_dlp import YoutubeDL
 from gfile import GFile
 
 @tree.command(name="youtube_dowonload", description="Download YouTube video.")
 async def download_youtube_command(interaction: discord.Interaction, url:str):
-    await interaction.response.send_message('処理中...')
-    # youtubeダウンロード
+    await interaction.response.send_message('リクエストを処理中...')
     opt = {
             'outtmpl': './download/%(title)s.%(ext)s',
             'format': 'best',
@@ -32,11 +33,8 @@ async def download_youtube_command(interaction: discord.Interaction, url:str):
     with YoutubeDL(opt) as ydl:
         inf = ydl.extract_info(url, download=True)
         path = ydl.prepare_filename(inf)
-    # gigafileへアップロード
     gfile_url = GFile(path, progress=False).upload().get_download_page()
-    # 一時ファイルの削除
     os.remove(path)
-
-    await interaction.followup.send(content=f'ダウンロード完了！\nダウンロードは[こちら]({gfile_url})')
+    await interaction.followup.edit_message(content=f'処理が完了しました。[ダウンロード]({gfile_url})')
 
 client.run(os.environ['APP_BOT_TOKEN'])
